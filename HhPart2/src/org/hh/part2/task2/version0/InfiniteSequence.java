ee4b64fa6f933b4730c67c5e3ac627e1c323f5df
Tkalenko Kirill Work <tkalkirill@yandex.ru>
2017-10-09 10:04:54 +0300

    task2 final prog
    task2 final prog

--- HhPart2/src/org/hh/part2/task2/version0/InfiniteSequence.java
+++ HhPart2/src/org/hh/part2/task2/version0/InfiniteSequence.java
@@ -1,19 +1,5 @@
 package org.hh.part2.task2.version0;
 
-/*
-Возьмём бесконечную цифровую последовательность,
-образованную склеиванием последовательных положительных чисел:
-S = 123456789101112131415...
-Определите первое вхождение заданной последовательности A
-в бесконечной последовательности S (нумерация начинается с 1).
-Пример входных данных:
-6789
-111
-Пример выходных данных:
-6
-12
- */
-
 public class InfiniteSequence {
 
     public interface Sequence {
@@ -25,10 +11,12 @@ public class InfiniteSequence {
         }
     }
 
+    /** Only for max_lenght_sequence({@link Integer#MAX_VALUE} - 2) */
     public static class SimpleSequence implements Sequence {
 
         private StringBuilder builder = new StringBuilder();
 
+        /**Have to maxValue < {@link Integer#MAX_VALUE} - 2)*/
         public SimpleSequence(int maxValue) {
             for (int i = 1; i <= maxValue; i++) {
                 this.builder.append(Integer.toString(i));
@@ -41,46 +29,48 @@ public class InfiniteSequence {
         }
     }
 
+    /** Base on math */
     public static class CalculateSequence implements Sequence {
 
         private static final long[][] VALUES = {
-                {9L, 1, 0L},
-                {99L, 2, 0L},
-                {999L, 3, 0L},
-                {9_999L, 4, 0L},
-                {99_999L, 5, 0L},
-                {999_999L, 6, 0L},
-                {9_999_999L, 7, 0L},
-                {99_999_999L, 8, 0L},
-                {999_999_999L, 9, 0L},
-                {9_999_999_999L, 10, 0L},
-                {99_999_999_999L, 11, 0L},
-                {999_999_999_999L, 12, 0L},
-                {9_999_999_999_999L, 13, 0L},
-                {99_999_999_999_999L, 14, 0L},
-                {999_999_999_999_999L, 15, 0L},
-                {9_999_999_999_999_999L, 16, 0L},
-                {99_999_999_999_999_999L, 17, 0L},
-                {999_999_999_999_999_999L, 18, 0L},
-                {Long.MAX_VALUE, 19, 0L},
+                { 9L, 1, 9L },
+                { 99L, 2, 0L },
+                { 999L, 3, 0L },
+                { 9_999L, 4, 0L },
+                { 99_999L, 5, 0L },
+                { 999_999L, 6, 0L },
+                { 9_999_999L, 7, 0L },
+                { 99_999_999L, 8, 0L },
+                { 999_999_999L, 9, 0L },
+                { 9_999_999_999L, 10, 0L },
+                { 99_999_999_999L, 11, 0L },
+                { 999_999_999_999L, 12, 0L },
+                { 9_999_999_999_999L, 13, 0L },
+                { 99_999_999_999_999L, 14, 0L },
+                { 999_999_999_999_999L, 15, 0L },
+                { 9_999_999_999_999_999L, 16, 0L },
+                { 99_999_999_999_999_999L, 17, 0L },
+                { 999_999_999_999_999_999L, 18, 0L },
+                { Long.MAX_VALUE, 19, 0L }
         };
 
-        private static String SINGLE_VALUE = "123456789";
+        private static final char[] SINGLE_VALUE = "123456789".toCharArray();
 
         static {
-            for (int i = 0, il = VALUES.length - 1; i < il; i++) {
-                VALUES[i][2] = VALUES[i][0] * VALUES[i][1];
+            for (int i = 1, j = 0, il = VALUES.length - 1; i < il; i++) {
+                VALUES[i][2] = (VALUES[i][0] - VALUES[i - 1][0]) * VALUES[i][1];
             }
         }
 
         @Override
         public char charAt(long index) {
-            if (index < 9)
-                return SINGLE_VALUE.charAt((int) index);
+            if (index < SINGLE_VALUE.length)
+                return SINGLE_VALUE[(int) index];
+            index++;
             long res = 0;
-            int i = 0;
-            for (; i < VALUES.length; i++) {
-                if (index > VALUES[i][0]) {
+            int i = 0, il = VALUES.length;
+            for (; i < il; i++) {
+                if (index > VALUES[i][2]) {
                     index -= VALUES[i][2];
                 } else {
                     res = index / VALUES[i][1];
@@ -88,7 +78,8 @@ public class InfiniteSequence {
                     index -= res * VALUES[i][1];
                     //cal res
                     res += VALUES[i - 1][0];
-                    if (index > 0) res++;
+                    if (index > 0)
+                        res++;
                     break;
                 }
             }
@@ -98,12 +89,11 @@ public class InfiniteSequence {
         }
     }
 
-    private static final Sequence SEQUENCE = new SimpleSequence(7000);
+    private static final Sequence SEQUENCE = new CalculateSequence();
 
     public long findSequence(String A) {
         int j = 0, jl = A.length();
-        MAIN:
-        for (long i = 0, il = Long.MAX_VALUE - A.length(); i < il; ) {
+        MAIN: for (long i = 0, il = Long.MAX_VALUE - A.length(); i < il;) {
             if (A.charAt(j) == SEQUENCE.charAt(i)) {
                 while (j < jl) {
                     if (A.charAt(j++) != SEQUENCE.charAt(i++)) {
--- HhPart2/test/org/hh/part2/task2/SequenceTest.java
+++ HhPart2/test/org/hh/part2/task2/SequenceTest.java
@@ -15,18 +15,16 @@ public class SequenceTest {
 
     @BeforeClass
     public static void beforeClass() {
-        maxValue = 7000;
+        maxValue = 10_000;
         simple = new InfiniteSequence.SimpleSequence(maxValue);
         calculate = new InfiniteSequence.CalculateSequence();
     }
 
     @Test
     public void test() {
-//        for (long i = 0, il = (long) maxValue; i <= il; i++) {
-//            Assert.assertEquals(simple.charAt(i), calculate.charAt(i));
-//        }
-        for (long i : new long[]{5, 6, 7, 17, 18, 19, 777, 778, 779}) {
-            Assert.assertEquals(simple.charAt(i), calculate.charAt(i));
+        for (long i = 0, il = (long) maxValue; i <= il; i++) {
+            Assert.assertEquals(String.format("error on index=[%s]", i),
+                simple.charAt(i), calculate.charAt(i));
         }
     }
 
